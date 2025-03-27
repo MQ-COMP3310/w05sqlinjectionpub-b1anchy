425a93b321e2c81857b6ddf89b3b1a1c52d21c86
b1anchy <ewancblanch@gmail.com>
2025-03-27 14:21:45 +1100

    fixed input validation

--- README.md
+++ README.md
@@ -1,9 +1,6 @@
 ﻿# Workshop Week 5
-<<<<<<< HEAD
 
 ## SQL Injections - Varun Irani
 
 ## Ewan's version
-=======
  ## SQL Injections
->>>>>>> parent of d22b1a3 (Changed README.md and using installed Gradle 8.12.1)
--- app/src/main/java/workshop05code/App.java
+++ app/src/main/java/workshop05code/App.java
@@ -72,11 +72,13 @@ public class App {
         try (Scanner scanner = new Scanner(System.in)) {
             System.out.print("Enter a 4 letter word for a guess or q to quit: ");
             String guess = scanner.nextLine();
-
             while (!guess.equals("q")) {
-                System.out.println("You've guessed '" + guess+"'.");
 
-                if (wordleDatabaseConnection.isValidWord(guess)) { 
+                System.out.println("You've guessed '" + guess+"'.");
+                if(!guess.matches("[a-z]") && guess.length() != 4) {
+                    System.out.println("Unacceptable input.");
+                }
+                else if (wordleDatabaseConnection.isValidWord(guess)) { 
                     System.out.println("Success! It is in the the list.\n");
                 }else{
                     System.out.println("Sorry. This word is NOT in the the list.\n");
